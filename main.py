147495240955b226b69472cf30b5d0f7ab3ccbd3
Abylay <abylay.kairatbek@nu.edu.kz>
2022-11-21 02:38:30 +0600

    feat: disease type rest

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from typing import List
+from urllib import response
+
 from fastapi import FastAPI, HTTPException
+from pydantic.generics import GenericModel
 from sqlalchemy import null
+from starlette import status
 from starlette.middleware.cors import CORSMiddleware
 
 import models
@@ -24,8 +29,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class PostResponse(GenericModel):
+    message: str
+    success: bool
+
 ###################################################################################
-@app.get("/diseasetypes", response_model=schemas.DiseaseType)
+@app.get("/diseasetypes", response_model=List[schemas.DiseaseType])
 def get_all_diseasetypes():
     dtypes = db.query(models.DiseaseType).all()
     if dtypes is None:
@@ -39,17 +48,85 @@ def get_diseasetype_id (id: int):
         raise HTTPException(status_code=404, detail="DiseaseType not found")
     return dtype
 
-# @app.post("/diseasetypes", status_code=201)
-# def create_diseasetype(dtype: schemas.DiseaseType):
+@app.post("/diseasetypes", response_model=PostResponse)
+def create_diseasetype(dtype: schemas.DiseaseType):
+    new_dtype = models.DiseaseType(id=dtype.id, description=dtype.description)
+    db_item = db.query(models.DiseaseType).filter(models.DiseaseType.id == dtype.id).first()
+    if db_item is not None:
+        raise HTTPException(status_code=400, detail="DiseaseType already exists")
+    db.add(new_dtype)
+    db.commit()
+    db.refresh(new_dtype)
+    return PostResponse(message="DiseaseType created successfully", success=True)
 
+@app.put("/diseasetypes/{id}", response_model=PostResponse)
+def update_diseasetype(id: int, dtype: schemas.DiseaseType):
+    db_item = db.query(models.DiseaseType).filter(models.DiseaseType.id == id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="DiseaseType not found")
+    db_item.description = dtype.description
+    db.commit()
+    db.refresh(db_item)
+    return PostResponse(message="DiseaseType updated successfully", success=True)
 
+@app.delete("/diseasetypes/{id}", response_model=PostResponse)
+def delete_diseasetype(id: int):
+    db_item = db.query(models.DiseaseType).filter(models.DiseaseType.id == id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="DiseaseType not found")
+    db.delete(db_item)
+    db.commit()
+    return PostResponse(message="DiseaseType deleted successfully", success=True)
 
 ###################################################################################
 @app.get("/country")
 def get_all_country():
     return db.query(models.Country).all()
 
+@app.get("/country/{cname}", response_model=schemas.Country)
+def get_country_cname (cname: str):
+    country =  db.query(models.Country).filter(models.Country.cname == cname).first()
+    if country is None:
+        raise HTTPException(status_code=404, detail="Country not found")
+    return country
 
+
+@app.post("/country", status_code=status.HTTP_201_CREATED, response_model=PostResponse)
+def create_country(country: schemas.Country):
+    db_country = models.Country(**country.dict())
+    db_item = db.query(models.Country).filter(models.Country.cname == db_country.cname).first()
+    if db_item is not None:
+        raise HTTPException(status_code=400, detail="Country already exists")
+
+    db.add(db_country)
+    db.commit()
+    db.refresh(db_country)
+    return PostResponse(message="Country created successfully", success=True)
+
+@app.put("/country/{cname}", status_code=status.HTTP_202_ACCEPTED, response_model=PostResponse)
+def update_country(cname: str, country: schemas.Country):
+    db_country = db.query(models.Country).filter(models.Country.cname == cname).first()
+    if db_country is None:
+        raise HTTPException(status_code=404, detail="Country not found")
+
+    db_country.cname = country.cname
+    db_country.population = country.population
+    db.commit()
+    return PostResponse(message="Country updated successfully", success=True)
+
+@app.delete("/country/{cname}", status_code=status.HTTP_202_ACCEPTED, response_model=PostResponse)
+def delete_country(cname: str):
+    db_country = db.query(models.Country).filter(models.Country.cname == cname).first()
+    if db_country is None:
+        raise HTTPException(status_code=404, detail="Country not found")
+
+    db.delete(db_country)
+    db.commit()
+    return PostResponse(message="Country deleted successfully", success=True)
+
+
+
+###################################################################################
 @app.get("/disease")
 def get_all_disease():
     return db.query(models.Disease).all()
--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 from sqlalchemy.sql.expression import null
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean, BigInteger
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, BigInteger
 from database import Base
+
 
 class DiseaseType(Base):
     __tablename__ = "diseasetype"
     id = Column(Integer, primary_key=True)
     description = Column(String(140), nullable=False)
+
 
 class Country(Base):
     __tablename__ = "country"
@@ -18,8 +20,7 @@ class Disease(Base):
     disease_code = Column(String(50), primary_key=True)
     pathogen = Column(String(20), nullable=False)
     description = Column(String(140), nullable=False)
-    id = Column(Integer, ForeignKey("diseasetype.id"), nullable=False)
-
+    id = Column(Integer, ForeignKey("diseasetype.id", ondelete='CASCADE'), nullable=True, unique=True)
 
 
 class Discover(Base):
@@ -29,16 +30,14 @@ class Discover(Base):
     first_enc_date = Column(DateTime, nullable=False)
 
 
-
 class Users(Base):
     __tablename__ = "users"
-    email = Column(String(60), primary_key=True)
+    email = Column(String(60), primary_key=True, unique=True)
     name = Column(String(30), nullable=False)
     surname = Column(String(40), nullable=False)
     salary = Column(Integer, nullable=False)
-    phone = Column(String(20), nullable=False)
+    phone = Column(String(20), nullable=False, unique=True)
     cname = Column(String(50), ForeignKey("country.cname"), nullable=False)
-
 
 
 class PublicServant(Base):
@@ -47,19 +46,16 @@ class PublicServant(Base):
     department = Column(String(50), nullable=False)
 
 
-
 class Doctor(Base):
     __tablename__ = "doctor"
     email = Column(String(60), ForeignKey("users.email"), primary_key=True)
     degree = Column(String(20), nullable=False)
 
 
-
 class Specialize(Base):
     __tablename__ = "specialize"
     id = Column(Integer, ForeignKey("diseasetype.id"), primary_key=True)
     email = Column(String(60), ForeignKey("doctor.email"), primary_key=True)
-
 
 
 class Record(Base):
@@ -69,4 +65,3 @@ class Record(Base):
     disease_code = Column(String(50), ForeignKey("disease.disease_code"), primary_key=True)
     total_deaths = Column(Integer, nullable=False)
     total_patients = Column(Integer, nullable=False)
-
