fd9faf3bd1790c778db9b10ca603a1d92d9fbd94
Abylay <abylay.kairatbek@nu.edu.kz>
2022-11-21 17:13:57 +0600

    feat: until users endpoint

--- main.py
+++ main.py
@@ -130,3 +130,108 @@ def delete_country(cname: str):
 @app.get("/disease")
 def get_all_disease():
     return db.query(models.Disease).all()
+
+@app.get("/disease/{disease_code}", response_model=schemas.Disease)
+def get_disease_by_id (disease_code: str):
+    disease =  db.query(models.Disease).filter(models.Disease.disease_code == disease_code).first()
+    if disease is None:
+        raise HTTPException(status_code=404, detail="Disease not found")
+    return disease
+
+@app.post("/disease", status_code=status.HTTP_201_CREATED, response_model=PostResponse)
+def create_disease(disease: schemas.Disease):
+    db_disease = models.Disease(**disease.dict())
+    db_item = db.query(models.Disease).filter(models.Disease.disease_code == db_disease.disease_code).first()
+    if db_item is not None:
+        raise HTTPException(status_code=400, detail="Disease already exists")
+
+    db.add(db_disease)
+    db.commit()
+    db.refresh(db_disease)
+    return PostResponse(message="Disease created successfully", success=True)
+
+@app.put("/disease/{disease_code}", status_code=status.HTTP_202_ACCEPTED, response_model=PostResponse)
+def update_disease(disease_code: str, disease: schemas.Disease):
+    db_disease = db.query(models.Disease).filter(models.Disease.disease_code == disease_code).first()
+    if db_disease is None:
+        raise HTTPException(status_code=404, detail="Disease not found")
+
+    db_disease.disease_code = disease.disease_code
+    db_disease.pathogen = disease.pathogen
+    db_disease.disease_name = disease.disease_name
+    db_disease.disease_type_id = disease.disease_type_id
+    db.commit()
+    return PostResponse(message="Disease updated successfully", success=True)
+
+@app.delete("/disease/{disease_code}", status_code=status.HTTP_202_ACCEPTED, response_model=PostResponse)
+def delete_disease(disease_code: int):
+    db_disease = db.query(models.Disease).filter(models.Disease.id == id).first()
+    if db_disease is None:
+        raise HTTPException(status_code=404, detail="Disease not found")
+
+    db.delete(db_disease)
+    db.commit()
+    return PostResponse(message="Disease deleted successfully", success=True)
+
+
+
+###################################################################################
+@app.get("/discover")
+def get_all_discover():
+    return db.query(models.Discover).all()
+
+@app.post("/discover", status_code=status.HTTP_201_CREATED, response_model=PostResponse)
+def create_discover(discover: schemas.Discover):
+    db_discover = models.Discover(**discover.dict())
+    db.add(db_discover)
+    db.commit()
+    db.refresh(db_discover)
+    return PostResponse(message="Discover created successfully", success=True)
+
+@app.get("/countrynamelist")
+def get_countrynamelist():
+    return db.query(models.Country.cname).all()
+
+@app.get("/diseasecodelist")
+def get_diseasecodelist():
+    return db.query(models.Disease.disease_code).all()
+
+
+@app.get('/users', response_model=List[schemas.Users])
+def get_all_users():
+    return db.query(models.Users).all()
+
+@app.post('/users', response_model=PostResponse)
+def create_user(user: schemas.Users):
+    db_user = models.Users(**user.dict())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return PostResponse(message="User created successfully", success=True)
+
+@app.delete('/users/{email}', response_model=PostResponse)
+def delete_user(email: str):
+    db_user = db.query(models.Users).filter(models.Users.email == email).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(db_user)
+    db.commit()
+    return PostResponse(message="User deleted successfully", success=True)
+
+@app.put('/users/{email}', response_model=PostResponse)
+def update_user(email: str, user: schemas.Users):
+    db_user = db.query(models.Users).filter(models.Users.email == email).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db_user.email = user.email
+    db_user.name = user.name
+    db_user.surname = user.surname
+    db_user.salary = user.salary
+    db_user.phone = user.phone
+    db_user.cname = user.cname
+    db.commit()
+    return PostResponse(message="User updated successfully", success=True)
+
+
